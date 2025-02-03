233d42ec1c80ea89be0672d42df2f12854420d1f
James Mitchell-White <jamesjimitchell@gmail.com>
2025-02-03 12:52:46 +0000

    another failed attempt
    This time, the problem is that the volume gets too big

--- embeddings/load_embeddings.py
+++ embeddings/load_embeddings.py
@@ -1,45 +1,68 @@
 from os import getenv
-import duckdb
+import adbc_driver_postgresql.dbapi
+import psycopg2
+from pgvector.psycopg2 import register_vector
+from tqdm import tqdm
+import polars as pl
+import pyarrow.parquet as pq
 
 
-vector_length = duckdb.sql(
-    """
-    SELECT array_length(embeddings) 
-    FROM 'embeddings/embeddings.parquet'
-"""
-).fetchone()[0]
+# uri = f"postgresql://{getenv('DB_USER')}:{getenv('DB_PASSWORD')}@{getenv('DB_HOST')}:{getenv('DB_PORT')}/{getenv('DB_NAME')}"
+uri = "postgresql://postgres:password@localhost:5432/omop"
 
+with psycopg2.connect(uri) as con:
+    print("\nConnected to database\n")
+    register_vector(con)
+    cursor = con.cursor()
+    parquet_file = pq.ParquetFile("embeddings.parquet")
+    print("Got parquet")
 
-with duckdb.connect() as con:
-    con.sql(
+    embeddings = pl.scan_parquet("embeddings.parquet")
+    vector_length = embeddings.first().collect().get_column("embeddings")[0].shape[0]
+
+    cursor.execute(
         f"""
-    INSTALL postgres;
-    LOAD postgres;
-    ATTACH '
-    dbname={getenv("DB_NAME")}
-    host={getenv("DB_HOST")}
-    port={getenv("DB_PORT")}
-    user={getenv("DB_USER")}
-    password={getenv("DB_PASSWORD")}
-    ' AS db (TYPE postgres, SCHEMA {getenv("SCHEMA_NAME")})
+        CREATE TEMP TABLE temp_parquet_data (
+            concept_id INTEGER,
+            embeddings vector({vector_length})
+        );
     """
     )
 
-    con.sql(
-        f"""
-    ALTER TABLE db.concept
-    ADD COLUMN IF NOT EXISTS embedding vector({vector_length})
-    """
-    )
+    con.commit()
 
-    con.sql(
-        """
-    UPDATE db.concept c
-    SET embedding = e.embeddings
-    FROM (
-        SELECT concept_id, embeddings 
-        FROM 'embeddings/embeddings.parquet'
-    ) e
-    WHERE c.concept_id = e.concept_id
-    """
-    )
+    # Each row will occupy 8514-ish bytes at the end
+    # To keep the memory usage below 4 Gb, setting the batch size to 200_000
+    for n, batch in tqdm(
+        enumerate(parquet_file.iter_batches(batch_size=200000))
+    ):  # Iterate through batches
+        print(f"\nProcessing batch {n}")
+        (
+            pl.DataFrame(batch)
+            .select(["concept_id", "embeddings"])
+            .with_columns(
+                pl.format(
+                    "[{}]",
+                    pl.col("embeddings").cast(pl.List(pl.String)).list.join(","),
+                )
+            )
+            .write_database(
+                table_name="cdm.temp_parquet_data",
+                connection=uri,
+                engine="adbc",
+                if_table_exists="append",
+            )
+        )
+
+        print(f"Processed batch {n}")
+        con.commit()
+
+        cursor.execute(
+            """
+            UPDATE cdm.concept c
+                SET embedding = tpd.embeddings
+                FROM temp_parquet_data tpd
+                WHERE c.concept_id = tpd.concept_id;
+            """
+        )  # Commit after processing each batch for performance
+        con.close()
