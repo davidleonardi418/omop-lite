78850655847eed4e3c6ea6e6467b67cb55df1899
James Mitchell-White <jamesjimitchell@gmail.com>
2025-02-04 14:53:48 +0000

    Fixed load_embeddings
    Now the memory usage can be better tuned

--- embeddings/load_embeddings.py
+++ embeddings/load_embeddings.py
@@ -1,68 +1,66 @@
 from os import getenv
-import adbc_driver_postgresql.dbapi
-import psycopg2
-from pgvector.psycopg2 import register_vector
-from tqdm import tqdm
-import polars as pl
 import pyarrow.parquet as pq
+import polars as pl
+from pgvector.psycopg import register_vector
+from tqdm import tqdm
+import psycopg
 
+uri = f"postgresql://{getenv('DB_USER')}:{getenv('DB_PASSWORD')}@{getenv('DB_HOST')}:{getenv('DB_PORT')}/{getenv('DB_NAME')}"
 
-# uri = f"postgresql://{getenv('DB_USER')}:{getenv('DB_PASSWORD')}@{getenv('DB_HOST')}:{getenv('DB_PORT')}/{getenv('DB_NAME')}"
-uri = "postgresql://postgres:password@localhost:5432/omop"
+vector_length = (
+    pl.scan_parquet("embeddings/embeddings.parquet")
+    .first()
+    .collect()
+    .get_column("embeddings")[0]
+    .shape[0]
+)
 
-with psycopg2.connect(uri) as con:
-    print("\nConnected to database\n")
-    register_vector(con)
-    cursor = con.cursor()
-    parquet_file = pq.ParquetFile("embeddings.parquet")
-    print("Got parquet")
+conn = psycopg.connect(uri)
+print("Connected to database\n")
+register_vector(conn)
+print("Registered vector type")
+cursor = conn.cursor()
 
-    embeddings = pl.scan_parquet("embeddings.parquet")
-    vector_length = embeddings.first().collect().get_column("embeddings")[0].shape[0]
-
-    cursor.execute(
-        f"""
-        CREATE TEMP TABLE temp_parquet_data (
-            concept_id INTEGER,
-            embeddings vector({vector_length})
-        );
+cursor.execute(
+    f"""
+    ALTER TABLE cdm.concept
+    ADD COLUMN IF NOT EXISTS embeddings vector({vector_length});
     """
-    )
+)
 
-    con.commit()
+conn.commit()
 
-    # Each row will occupy 8514-ish bytes at the end
-    # To keep the memory usage below 4 Gb, setting the batch size to 200_000
-    for n, batch in tqdm(
-        enumerate(parquet_file.iter_batches(batch_size=200000))
-    ):  # Iterate through batches
-        print(f"\nProcessing batch {n}")
-        (
-            pl.DataFrame(batch)
-            .select(["concept_id", "embeddings"])
-            .with_columns(
-                pl.format(
-                    "[{}]",
-                    pl.col("embeddings").cast(pl.List(pl.String)).list.join(","),
-                )
-            )
-            .write_database(
-                table_name="cdm.temp_parquet_data",
-                connection=uri,
-                engine="adbc",
-                if_table_exists="append",
+# Open the Parquet file in streaming mode
+parquet_file = pq.ParquetFile("embeddings/embeddings.parquet")
+
+# Each row will occupy 8514-ish bytes at the end
+# To keep the memory usage below 4 Gb, setting the batch size to 200_000
+for batch in tqdm(parquet_file.iter_batches(batch_size=200000)):
+    df = (
+        pl.DataFrame(batch)
+        .select(["concept_id", "embeddings"])
+        .with_columns(
+            pl.format(
+                "[{}]",
+                pl.col("embeddings")
+                .cast(pl.List(pl.String))
+                .list.join(",")
+                .alias("embeddings"),
             )
         )
+    )
+    chunk_size = 10000
+    update_query = """
+        UPDATE cdm.concept
+            SET embeddings = %s
+            WHERE concept_id = %s"""
 
-        print(f"Processed batch {n}")
-        con.commit()
-
-        cursor.execute(
-            """
-            UPDATE cdm.concept c
-                SET embedding = tpd.embeddings
-                FROM temp_parquet_data tpd
-                WHERE c.concept_id = tpd.concept_id;
-            """
-        )  # Commit after processing each batch for performance
-        con.close()
+    for row_start in range(0, len(df), chunk_size):
+        d_list = [
+            (row[1], row[0])
+            for row in df[row_start : row_start + chunk_size].iter_rows()
+        ]
+        cursor.executemany(update_query, d_list)
+        conn.commit()
+        print(f"Committed {chunk_size} entries")
+conn.close()
